c7c34fc986e4ee9054717ac4a685c9caffb3ea4d
ALEX NEVES <alexprogran@gmail.com>
2023-01-30 08:31:40 -0300

    Introduzindo o arquivo main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+
+
+
+print("CALCULADORA")
+if __name__ =="__main__":
+    opcao = 1
+    while opcao != 3:
+        print("CALCULADORA GIT")
+        print(30*("="))
+        print('1 - SOMA')
+        print("2-SUBTRAÇÃO")
+        print("3 - SAIR")
+        print(30*("="))
+        opcao = int(input("OPCAO: "))
+
+        if opcao == 1:           
+            print( " OPÇAO SOMA")
+        elif opcao == 2:
+            print("OPÇÃO SUBTRAÇÃO.")
+        elif opcao == 3:
+            print("OBRIGADO POR USAR A NOSSA  CALCULADORA: ")
